7e307ca5f61f36cd31387f045f2a63846c9da248
Hadad Al Gojali <hadadalgojali@gmail.com>
2019-10-14 09:48:21 +0700

    Config Https

--- application/controllers/Vclaim.php
+++ application/controllers/Vclaim.php
@@ -29,10 +29,14 @@ class Vclaim extends CI_Controller {
 
 		$url = $conf_app['bpjs']['url_rujukan'].$this->input->post('no_signature');
 		$curl = curl_init();
+		curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+		curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+		curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
 		curl_setopt($curl, CURLOPT_URL, $url);
 		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 		curl_setopt($curl, CURLOPT_HEADER, false);
 		curl_setopt($curl, CURLOPT_HTTPHEADER, $this->getSignature()); 
+
 		$data = curl_exec($curl);
 		echo $data;
 	}
